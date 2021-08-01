15d5ead597a68ce5d5b5f0f0a87293f984100c95
icocampos <icarocampos97@gmail.com>
2021-07-31 21:27:53 -0300

    error boundary

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ErrorBoundary from './ErrorBoundary';
+
+const main = (
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+)
 
 ReactDOM.render(
   <React.StrictMode>
